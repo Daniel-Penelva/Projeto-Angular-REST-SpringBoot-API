75e5a0a3a6810160a6b4a9f530480ec5e657e23f
Daniel Penelva <d4n.andrade@gmail.com>
2023-08-02 22:51:34 -0300

    Criando a Barra de Menu e criando o router sair e home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'Projeto-Angular-REST';
 
-  constructor(){}
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+    if(localStorage.getItem('token') == null){ // Se não tiver o token é redirecionado para a página login
+      this.router.navigate(['login']);
+    }
+  }
+
+  // Método para sair da sessão de logado e ir para página de login
+  public sair(){
+    localStorage.clear();
+    this.router.navigate(['login']);
+  }
+  
   
 }
